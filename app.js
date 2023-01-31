07210a4e949f579c75832855ccf906bb3f3b5f33
MMhaidari <99756088+MMhaidari@users.noreply.github.com>
2023-01-31 15:23:50 +0430

    changes added to navbar

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const navigationLinks = document.querySelector('.navigation-links');
 const menuBtn = document.querySelector('.humbarger-btn');
-
-// document.addEventListener("DOMContentLoaded", (e) => {
-//   navigationLinks.style.display = "none";
-// });
+const bodyElement = document.querySelector('body');
+const navigationLinksEl = document.querySelector('.navigation-links > a');
 
 menuBtn.addEventListener('click', (e) => {
   e.stopPropagation();
@@ -14,7 +12,15 @@ menuBtn.addEventListener('click', (e) => {
 function updateMenuBtnImage() {
   if (navigationLinks.classList.contains('open-nav')) {
     menuBtn.setAttribute('src', 'mobile-ingrediants/Union.png');
+    bodyElement.classList.remove('hidescrollbar');
   } else {
     menuBtn.setAttribute('src', './mobile-ingrediants/Icon.png');
+    bodyElement.classList.add('hidescrollbar');
   }
 }
+
+navigationLinks.addEventListener('click', function () {
+  navigationLinks.classList.add('open-nav');
+  bodyElement.classList.remove('hidescrollbar');
+  menuBtn.setAttribute('src', 'mobile-ingrediants/Union.png');
+});
