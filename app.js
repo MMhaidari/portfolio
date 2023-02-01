83e5f3dfc530f3534c4b450ff96fbfe20967d743
MMhaidari <99756088+MMhaidari@users.noreply.github.com>
2023-02-01 16:42:59 +0430

    form validation js code first event section is added

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const menuBtn = document.querySelector('.humbarger-btn');
 const bodyElement = document.querySelector('body');
 const navigationLinksEl = document.querySelector('.navigation-links > a');
 
+const contactForm = document.getElementById('contact-form');
+const error = document.querySelector('.error');
+
 menuBtn.addEventListener('click', (e) => {
   e.stopPropagation();
   navigationLinks.classList.toggle('open-nav');
@@ -24,3 +27,15 @@ navigationLinks.addEventListener('click', function () {
   bodyElement.classList.remove('hidescrollbar');
   menuBtn.setAttribute('src', 'mobile-ingrediants/Union.png');
 });
+
+contactForm.addEventListener('click', function () {
+  const email = contactForm.elements['email'].value;
+  const error = document.querySelector('.error');
+});
+
+//   if (email !== email.toLowerCase()) {
+//   error.style.cssText = `visibility: visible; opacity: 1`;
+//   error.innerHTML = 'Please enter email in lowercase';
+// } else {
+//   error.style.cssText = `visibility: hidden; opacity: 0`;
+// }
