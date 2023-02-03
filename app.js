9cd52a822b06a3051731adf2d94ea1d6b7699d4c
MMhaidari <99756088+MMhaidari@users.noreply.github.com>
2023-02-03 21:31:54 +0430

    the pop window final touches added

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const navigationLinks = document.querySelector('.navigation-links');
 const menuBtn = document.querySelector('.humbarger-btn');
 const bodyElement = document.querySelector('body');
-const navigationLinksEl = document.querySelector('.navigation-links > a');
-
 const contactForm = document.getElementById('contact-form');
-const error = document.querySelector('.error');
+const submitBtn = document.querySelector('.btn-submit');
 
 menuBtn.addEventListener('click', (e) => {
   e.stopPropagation();
@@ -22,19 +20,46 @@ function updateMenuBtnImage() {
   }
 }
 
-navigationLinks.addEventListener('click', function () {
+navigationLinks.addEventListener('click', () => {
   navigationLinks.classList.add('open-nav');
   bodyElement.classList.remove('hidescrollbar');
   menuBtn.setAttribute('src', 'mobile-ingrediants/Union.png');
 });
 
-contactForm.addEventListener('click', function () {
-  const email = contactForm.elements['email'].value;
+submitBtn.addEventListener('click', (event) => {
+  const email = contactForm.elements.email.value;
   const error = document.querySelector('.error');
   if (email !== email.toLowerCase()) {
-    error.style.cssText = `visibility: visible; opacity: 1`;
+    event.preventDefault();
+    error.style.cssText = 'visibility: visible; opacity: 1';
     error.innerHTML = 'Please enter email in lowercase';
   } else {
-    error.style.cssText = `visibility: hidden; opacity: 0`;
+    error.style.cssText = 'visibility: hidden; opacity: 0';
   }
+});
+
+// Data preserve
+const contactDetails = {
+  fullName: String,
+  email: String,
+  message: String,
+};
+
+window.onload = () => {
+  const contactData = JSON.parse(localStorage.getItem('contactDetails'));
+
+  //populate the local storage information into the contact form
+  if (contactData) {
+    document.getElementById('name').value = contactData.fullName;
+    document.getElementById('email').value = contactData.email;
+    document.getElementById('msg').value = contactData.message;
+  }
+};
+
+contactForm.addEventListener('change', () => {
+  contactDetails.fullName = document.getElementById('name').value;
+  contactDetails.email = document.getElementById('email').value;
+  contactDetails.message = document.getElementById('msg').value;
+  //set the local storage data with the upddated contact form
+  localStorage.setItem('contactDetails', JSON.stringify(contactDetails));
 });
--- project.js
+++ project.js
@@ -1,5 +1,147 @@
-const viewBtn = document.getElementsByClassName('btn-view');
+const portfolioItems = [
+  {
+    name: 'Tonic',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: 'desktop-ingrediants/card.png',
+    technologies: ['html', 'css', 'javascript'],
+    liveLink: '',
+    sourceLink: '',
+    category: 'CANOPY',
+    stack: 'Back End Dev',
+    year: '2015',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    featuredImage: 'desktop-ingrediants/card-1.png',
+    technologies: ['html', 'css', 'javascript'],
+    liveLink: '',
+    sourceLink: '',
+    category: 'FACEBOOK',
+    stack: 'Full Stack Dev',
+    year: '2015',
+  },
+  {
+    name: 'Facebook 360',
+    description:
+      "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    featuredImage: 'desktop-ingrediants/card-2.png',
+    technologies: ['html', 'css', 'javascript'],
+    liveLink: '',
+    sourceLink: '',
+    category: 'FACEBOOK',
+    stack: 'Full Stack Dev',
+    year: '2015',
+  },
+  {
+    name: 'Uber Navigation',
+    description:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    featuredImage: 'desktop-ingrediants/card-3.png',
+    technologies: ['html', 'css', 'javascript'],
+    liveLink: '',
+    sourceLink: '',
+    category: 'Uber',
+    stack: 'Lead Developer',
+    year: '2018',
+  },
+];
 
-viewBtn.addEventListener('click', function () {
-  console.log('cliked');
+const cardProject = document.querySelector('#section-projects');
+
+let portfolioSection = '';
+portfolioItems.forEach((item, index) => {
+  let technologies = '';
+  item.technologies.forEach((tech) => {
+    technologies += `<li>${tech}</li>`;
+  });
+  const portfolioItem = `
+  <div class="project">
+  <div class="first-col">
+    <img class="card-img" src="${item.featuredImage}" alt="project demo image" />
+  </div>
+  <div class="project-container">
+    <h3 class="project-card-header">Tonic</h3>
+    <div class="project-info">
+      <h4>${item.name}</h4>
+      <img class="dot-icon" src="mobile-ingrediants/Counter.png" alt="icon" />
+      <h4 class="project-card-header-2">${item.category}</h4>
+      <img class="dot-icon" src="mobile-ingrediants/Counter.png" alt="icon" />
+      <h4 class="project-card-header-2">${item.year}</h4>
+    </div>
+    <p class="card-decrption">
+      ${item.description}
+    </p>
+    <ul class="programming-lang">
+    ${technologies}
+    </ul>
+    </ul>
+    <button id="portfolio-button-${index}" class="btn see-project-button">See Project</button>
+  </div>
+</div>`;
+  portfolioSection += portfolioItem;
+});
+
+cardProject.innerHTML += portfolioSection;
+
+const seeProjectButtons = document.querySelectorAll('.see-project-button');
+
+const popupContainer = document.querySelector('#popup');
+seeProjectButtons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const buttonIndex = Number(e.target.id.split('-')[2]);
+    const portfolioItem = portfolioItems[buttonIndex];
+    let technologies = '';
+    portfolioItem.technologies.forEach((tech) => {
+      technologies += `<li>${tech}</li>`;
+    });
+
+    popupContainer.innerHTML = `
+    <div class="overlay">
+    <div class="popup-wrapper">
+    <div class="model first-card-model">
+      <div class="model__header_container">
+        <h3 class="model__header">${portfolioItem.name}</h3>
+        <button type="button" title="close button" class="close_btn">
+          <img src="mobile-ingrediants/Icon.png" width="100%" alt="close button">
+        </button>
+      </div>
+      <div class="model-info">
+        <h4>${portfolioItem.category}</h4>
+        <img class="dot-icon" src="mobile-ingrediants/Counter.png" alt="icon" />
+        <h4 class="project-card-header-2">${portfolioItem.stack}</h4>
+        <img class="dot-icon" src="mobile-ingrediants/Counter.png" alt="icon" />
+        <h4 class="project-card-header-2">${portfolioItem.year}</h4>
+      </div>
+      <img class="model-img" src="${portfolioItem.featuredImage}" alt="project demo image" />
+      <div class="model-info-desription">
+      <p class="model-decrption">
+      ${portfolioItem.description}
+      </p>
+      <div>
+        <ul class="programming-lang">
+        ${technologies}
+        </ul>
+        <hr class="breakline">
+        <ul class="model_btn">
+        <li>
+          <a href="#" class="see-live">See live <img src="./desktop-ingrediants/Icon.png" alt="icon"></a>
+          </li>
+          <li>
+          <a href="#" class="see-source">See Source <img src="./desktop-ingrediants/Frame.png" alt="icon"></a>
+          </li>
+        </ul>
+      </div>
+      </div>
+    </div>
+  </div>
+  </div>`;
+    popupContainer.style.display = 'flex';
+    document.querySelector('.close_btn').addEventListener('click', () => {
+      popupContainer.style.display = 'none';
+      document.querySelector('body').classList.remove('hidescrollbar');
+    });
+  });
 });
